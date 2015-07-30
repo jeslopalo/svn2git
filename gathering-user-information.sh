8027c305032a1c2f2b7ce8ff542a5b47a3da2b2e
jeslopalo <jeslopalo@sandbox.es>
2015-07-30 14:35:52 +0200

    Extracting authors

--- gathering-user-information.sh
+++ gathering-user-information.sh
@@ -4,12 +4,12 @@ set -e
 
 red='\033[0;31m'
 green='\033[0;32m'
+yellow='\033[0;33m'
 NC='\033[0m' # No Color
 
 
 
-echo -e "\n\nWe are going to migrate a SVN repository to GIT!"
-
+echo -e "\n\nWe are going to extract commiters data from SVN repository!"
 
 
 confirmation="M"
@@ -22,9 +22,23 @@ while [ "$confirmation" != "Y" ] && [ "$confirmation" != "y" ]; do
 done
 
 if [ "$svn_url" == "" ]; then
-        echo -e "${red}¡GAME OVER!${NC} I need a SVN url"
+        echo -e "${red}GAME OVER!${NC} Sorry, an SVN url is mandatory!"
         exit
 fi
+
+#echo -e "${yellow} ${NC}..."
+echo -e "${yellow}Checking out from svn repository $svn_url in repo directory${NC}..."
+if [ -d "repo" ]; then
+        echo -e "${red}GAME OVER!${NC} Sorry, a ${green}repo${NC} directory already exists, please delete it and start agein."
+        exit
+fi
+svn co $svn_url repo
+
+echo -e "${yellow}Changing directory to `pwd`/repo ${NC}..."
+cd repo
+
+echo -e "${yellow}Extracting commit authors${NC}..."
+svn log -q | awk -F '|' '/^r/ {sub("^ ", "", $2); sub(" $", "", $2); print $2" = "$2" <"$2">"}' | sort -u > authors-transform.txt
 
 #echo -e "\nCreando el directorio ${green} $repository_name ${NC}..."
 #mkdir -p $repository_name
